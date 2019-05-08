1929a3bec5671f425cb7b860a6d498fb7e2e2235
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-05-09 00:10:16 +0200

    Simplify Components

--- src/js/core/renderRoot.jsx
+++ src/js/core/renderRoot.jsx
@@ -1,12 +1,10 @@
 import l from "utils";
 import Root from "../Components/Root/Root";
 
-const App = () => <Root />;
-
 const renderRoot = () =>
 	wp.customize.bind("ready", () =>
 		wp.element.render(
-			<App />,
+			<Root />,
 			document.getElementById("customize-control-control_thet")
 		)
 	);
