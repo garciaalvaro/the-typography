6eabdfdd78ef6c02694902e41a0ceca609063081
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-05-11 21:44:50 +0200

    Clean previewer script.

--- src/index-customizer-preview.js
+++ src/index-customizer-preview.js
@@ -2,65 +2,76 @@ import WebFont from "webfontloader";
 
 ($ => {
 	const l = console.log.bind(console); // Console log shortcut helper
-	let styles = [];
-	const api = wp.customize;
-	let $stylesheet_styles = $("#thet-styles");
 	const updateStylesheet = () => {
 		let css;
-		css = styles.map(({ selector, css }) => `${selector}{${css}}`);
+		css = state.styles.map(({ selector, css }) => `${selector}{${css}}`);
 		css = css.join("");
 
-		$stylesheet_styles.html(css);
+		state.$stylesheet.html(css);
+	};
+	const state = {
+		gfonts_loaded: [],
+		styles: [],
+		$stylesheet: null
 	};
 
 	// Create stylesheet if it doesn't exist yet.
-	if (!$stylesheet_styles.length) {
-		$stylesheet_styles = $("head")
+	if (!state.$stylesheet) {
+		state.$stylesheet = $("head")
 			.append('<style type="text/css" id="thet-styles" />')
 			.find("#thet-styles");
 	}
 
-	api.bind("preview-ready", () => {
-		// Listen to the customizer typography styles changes.
-		api.preview.bind(
-			"thet-add_modify_style",
-			({ id, typography_id, selector, css }) => {
-				if (styles.find(style => style.id === id)) {
-					styles = styles.map(style => {
-						if (style.id === id) {
-							return { id, typography_id, selector, css };
+	wp.customize.bind("preview-ready", () => {
+		// Listen to the customizer typography changes.
+		wp.customize.preview
+			// Style added or modified.
+			.bind("thet-add_modify_style", style_changed => {
+				if (state.styles.find(({ id }) => id === style_changed.id)) {
+					state.styles = state.styles.map(style => {
+						if (style.id === style_changed.id) {
+							return style_changed;
 						}
 
 						return style;
 					});
 				} else {
-					styles.push({ id, typography_id, selector, css });
+					state.styles.push(style_changed);
 				}
 
 				updateStylesheet();
-			}
-		);
-		api.preview.bind("thet-remove_style", typography_ids => {
-			styles = styles.filter(
-				({ typography_id }) => !typography_ids.includes(typography_id)
-			);
-
-			updateStylesheet();
-		});
-		api.preview.bind("thet-gfonts", families =>
-			WebFont.load({
-				google: {
-					families
-				}
 			})
-		);
-		// Send the post data from the current window to the customizer.
-		api.preview.bind("active", () => {
-			l("active");
-			the_typography.is_front_page = the_typography.is_front_page == true;
-			the_typography.is_404 = the_typography.is_404 == true;
+			// Style removed.
+			.bind("thet-remove_style", typography_ids => {
+				state.styles = state.styles.filter(
+					({ typography_id }) => !typography_ids.includes(typography_id)
+				);
 
-			api.preview.send("thet-page_data", the_typography);
-		});
+				updateStylesheet();
+			})
+			// Add font.
+			.bind("thet-add_gfonts", gfonts_to_load => {
+				// const families = gfonts_to_load.map(({ family, variants }) => {
+				// 	family = family.replace(/_/g, "+");
+				// 	variants = variants.join(",");
+				// 	return `${family}:${variants}`;
+				// });
+				// WebFont.load({
+				// 	google: {
+				// 		families
+				// 	},
+				// 	active: () => {
+				// 		loadGFonts(gfonts_to_load);
+				// 	}
+				// });
+			})
+			// Send the post data from the current window to the customizer.
+			.bind("active", () => {
+				l("active");
+				the_typography.is_front_page = the_typography.is_front_page == true;
+				the_typography.is_404 = the_typography.is_404 == true;
+
+				wp.customize.preview.send("thet-page_data", the_typography);
+			});
 	});
 })(jQuery);
