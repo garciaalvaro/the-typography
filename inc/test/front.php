01a5d9f819e3d0a0105ca5579bd94a216817553a
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-06-07 02:51:50 +0200

    Added PHP classes.

--- inc/test/front.php
+++ inc/test/front.php
@@ -5,21 +5,11 @@ namespace THETYPOGRAPHY;
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-// add_filter( 'wp_head', __NAMESPACE__ . '\test_front' );
+add_filter( 'wp_head', __NAMESPACE__ . '\test_front' );
 function test_front() {
 
-	$typography_args = array(
-		'post_type'  => 'the_typography',
-		'meta_query' => array(
-			array(
-				'key'     => 'id',
-				'value'   => 'aaa',
-				'compare' => '=',
-			)
-		)
-	);
+	$data = register_my_typography();
+	$data = prepare_groups( $data );
 
-	$typography_query = new \WP_Query( $typography_args );
-
-	var_dump($typography_query->posts[0]->ID);
+	highlight_string("<?php\n\$data =\n" . var_export($data, true) . ";\n?>");
 }
