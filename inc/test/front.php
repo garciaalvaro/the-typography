495aa0acdbd8008818220a592dad1209fee514ce
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-06-11 16:10:25 +0200

    Fix selector groups.

--- inc/test/front.php
+++ inc/test/front.php
@@ -8,18 +8,8 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 add_filter( 'wp_head', __NAMESPACE__ . '\test_front' );
 function test_front() {
 
-	// $data = register_my_typography();
-	// $data = prepare_groups( $data );
-	// $data='-123';
-	// $data=(integer)$data;
-	// $data = (string)round( floatval( $data ), 2 );
+	$data = get_option('qqq');
 
-	$qq = array('aa'=>123,'bb'=>456);
-
-	var_dump($qq);
-
-	$qq = array_merge(array('aa'=>789),$qq);
-
-	// var_dump($qq);
+	var_dump($data);
 
 }
