35ae2a56a6aaa5ef4e851aa4959ba159e4c0619f
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-06-12 00:39:27 +0200

    Styling fixes

--- inc/test/front.php
+++ inc/test/front.php
@@ -8,8 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 add_filter( 'wp_head', __NAMESPACE__ . '\test_front' );
 function test_front() {
 
-	$data = get_option('qqq');
-
-	var_dump($data);
+	// var_dump(\sanitize_text_field('Qqweqw - qwe'));
 
 }
