f63808b779dc148d9339f696731e3bf5ec83c721
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-06-13 11:05:16 +0200

    Remove predefined typographies that dont exist anymore.

--- inc/test/front.php
+++ inc/test/front.php
@@ -8,6 +8,6 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 add_filter( 'wp_head', __NAMESPACE__ . '\test_front' );
 function test_front() {
 
-	// var_dump(\sanitize_text_field('Qqweqw - qwe'));
+	var_dump(get_option('the_typography'));
 
 }
