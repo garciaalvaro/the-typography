bb24c8e04e2d40c1f130512d07c87f812bc2e55d
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-06-16 19:31:43 +0200

    Added TheTypography class in root file

--- the-typography.php
+++ the-typography.php
@@ -14,54 +14,82 @@ namespace THETYPOGRAPHY;
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
-if ( ! defined( __NAMESPACE__ . '\PLUGIN_VERSION' ) ) {
-	define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.1.0' );
-}
-if ( ! defined( __NAMESPACE__ . '\PLUGIN_NAME' ) ) {
-	define( __NAMESPACE__ . '\PLUGIN_NAME', 'the-typography' );
-}
-if ( ! defined( __NAMESPACE__ . '\BUILD_DIR' ) ) {
-	define( __NAMESPACE__ . '\BUILD_DIR', plugins_url( 'build/', __FILE__ ) );
-}
-if ( ! defined( __NAMESPACE__ . '\INC_DIR' ) ) {
-	define( __NAMESPACE__ . '\INC_DIR', plugin_dir_path( __FILE__ ) . 'inc/' );
-}
-if ( ! defined( __NAMESPACE__ . '\PLUGIN_DIR' ) ) {
-	define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-}
+if ( ! class_exists( 'TheTypography' ) ) {
 
-// Utils.
-require_once INC_DIR . 'utils-CastArray.php';
-require_once INC_DIR . 'utils-Sanitize.php';
-require_once INC_DIR . 'utils-Schema.php';
-require_once INC_DIR . 'utils-Defaults.php';
-require_once INC_DIR . 'utils-Utils.php';
+	/**
+	 * Class TheTypography
+	 */
+	class TheTypography {
 
-// Traits.
-require_once INC_DIR . 'trait-TypographyStyle.php';
+		function __construct() {
 
-// Classes.
-require_once INC_DIR . 'class-LoadTypographies.php';
-require_once INC_DIR . 'class-SelectorFront.php';
-require_once INC_DIR . 'class-SelectorGroupFront.php';
-require_once INC_DIR . 'class-TypographyFront.php';
+			$this->registerActivation();
+			$this->defineConstants();
+			$this->includeFiles();
+		}
 
-// Register.
-require_once INC_DIR . 'register-customizer.php';
-require_once INC_DIR . 'register-load_typographies.php';
-require_once INC_DIR . 'register-enqueue.php';
-require_once INC_DIR . 'register-cpt.php';
-require_once INC_DIR . 'register-taxonomy.php';
-require_once INC_DIR . 'register-meta.php';
+		private function registerActivation() {
+
+			register_activation_hook( __FILE__, __NAMESPACE__ . '\register_the_typography_cpt' );
+			register_activation_hook( __FILE__, __NAMESPACE__ . '\register_the_typography_taxonomy' );
+			register_activation_hook( __FILE__, __NAMESPACE__ . '\register_the_typography_taxonomy_terms' );
+		}
+
+		private function defineConstants() {
+
+			if ( ! defined( __NAMESPACE__ . '\PLUGIN_VERSION' ) ) {
+				define( __NAMESPACE__ . '\PLUGIN_VERSION', '1.1.0' );
+			}
+			if ( ! defined( __NAMESPACE__ . '\PLUGIN_NAME' ) ) {
+				define( __NAMESPACE__ . '\PLUGIN_NAME', 'the-typography' );
+			}
+			if ( ! defined( __NAMESPACE__ . '\BUILD_DIR' ) ) {
+				define( __NAMESPACE__ . '\BUILD_DIR', plugins_url( 'build/', __FILE__ ) );
+			}
+			if ( ! defined( __NAMESPACE__ . '\INC_DIR' ) ) {
+				define( __NAMESPACE__ . '\INC_DIR', plugin_dir_path( __FILE__ ) . 'inc/' );
+			}
+			if ( ! defined( __NAMESPACE__ . '\PLUGIN_DIR' ) ) {
+				define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+			}
+		}
+
+		private function includeFiles() {
+
+			// Utils.
+			require_once INC_DIR . 'utils-CastArray.php';
+			require_once INC_DIR . 'utils-Sanitize.php';
+			require_once INC_DIR . 'utils-Schema.php';
+			require_once INC_DIR . 'utils-Defaults.php';
+			require_once INC_DIR . 'utils-Utils.php';
+
+			// Traits.
+			require_once INC_DIR . 'trait-TypographyStyle.php';
+
+			// Classes.
+			require_once INC_DIR . 'class-LoadTypographies.php';
+			require_once INC_DIR . 'class-SelectorFront.php';
+			require_once INC_DIR . 'class-SelectorGroupFront.php';
+			require_once INC_DIR . 'class-TypographyFront.php';
+
+			// Register.
+			require_once INC_DIR . 'register-customizer.php';
+			require_once INC_DIR . 'register-load_typographies.php';
+			require_once INC_DIR . 'register-enqueue.php';
+			require_once INC_DIR . 'register-cpt.php';
+			require_once INC_DIR . 'register-taxonomy.php';
+			require_once INC_DIR . 'register-meta.php';
 
 // DEV_start
 require_once INC_DIR . 'test/back.php';
 require_once INC_DIR . 'test/front.php';
 // DEV_end
-// PRO_start
-require_once PLUGIN_DIR . 'pro/the-typography-pro.php';
-// PRO_end
 
-register_activation_hook( __FILE__, __NAMESPACE__ . '\register_the_typography_cpt' );
-register_activation_hook( __FILE__, __NAMESPACE__ . '\register_the_typography_taxonomy' );
-register_activation_hook( __FILE__, __NAMESPACE__ . '\register_the_typography_taxonomy_terms' );
+			if ( file_exists( PLUGIN_DIR . 'pro/the-typography-pro.php' ) ) {
+				require_once PLUGIN_DIR . 'pro/the-typography-pro.php';
+			}
+		}
+	}
+
+	new TheTypography();
+}
