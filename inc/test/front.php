518335f5c15ac09c34dae198effff1c2692cd78d
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-07-10 19:38:50 +0200

    Updated dependencies.

--- inc/test/front.php
+++ inc/test/front.php
@@ -8,10 +8,4 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 add_filter( 'wp_head', __NAMESPACE__ . '\test_front' );
 function test_front() {
 
-	// delete_option('the_typography');
-	// delete_transient( 'the_typography_is_saving');
-	// delete_option('qqq');
-	// var_dump(get_option('the_typography'));
-	// var_dump(get_option('qqq'));
-
 }
